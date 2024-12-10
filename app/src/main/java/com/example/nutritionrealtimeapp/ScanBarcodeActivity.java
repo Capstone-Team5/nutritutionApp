a75478905e8301a7e62ad388a5036a3ec3b39d58
aa <ge1235@naver.com>
2024-12-10 16:22:40 +0900

    오타

--- app/src/main/java/com/example/nutritionrealtimeapp/ScanBarcodeActivity.java
+++ app/src/main/java/com/example/nutritionrealtimeapp/ScanBarcodeActivity.java
@@ -1,6 +1,6 @@
 package com.example.nutritionrealtimeapp;
 
-dimport android.content.Context;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
